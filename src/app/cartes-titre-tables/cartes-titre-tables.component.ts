ca547465b6f03a354ed633a5cf68f1bc5b2ad972
Ariony92 <zkhan@iut.univ-paris8.fr>
2025-03-18 10:45:32 +0100

    Initialisation Composants

---
+++ src/app/cartes-titre-tables/cartes-titre-tables.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cartes-titre-tables',
+  imports: [],
+  templateUrl: './cartes-titre-tables.component.html',
+  styleUrl: './cartes-titre-tables.component.css'
+})
+export class CartesTitreTablesComponent {
+
+}
