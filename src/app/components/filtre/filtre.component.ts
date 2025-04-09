9ca1e48f977cf0432ef5953d72ee48bdec58a7d5
Karim-Bouqsi <pro.karimb@gmail.com>
2025-04-09 11:12:21 +0200

    fix tri par date

--- src/app/components/filtre/filtre.component.ts
+++ src/app/components/filtre/filtre.component.ts
@@ -24,32 +24,39 @@ export class FiltreComponent {
   private readonly tableService = inject(TableService);
 
   formualire() {
-    this.showForm = !this.showForm;  // Toggle pour afficher ou cacher le formulaire
+    this.showForm = !this.showForm;
   }
-
   onSubmit(form: NgForm){
     if (form.valid){
+      const handleResponse = (reponse: Insertions | any) => {
+        const donnees = (reponse as any).entry ?? reponse.data ?? [];
+        this.envoieDonnees.emit(donnees);
+      };
+  
       if (form.value.minValue || form.value.maxValue){
         if (form.value.minValue > form.value.maxValue){
           // afficher une erreur
+
         }
         else {
-          this.tableService.rechercheDate(this.nomTable, this.colonne, form.value.minValue, form.value.maxValue).subscribe({
-            next: (reponse: Insertions) => {
-              this.envoieDonnees.emit(reponse.entry);
-            }
-          })
+          this.tableService.rechercheDate(
+            this.nomTable,
+            this.colonne,
+            form.value.minValue,
+            form.value.maxValue
+          ).subscribe({ next: handleResponse });
         }
       }
       else {
-        this.tableService.recherche(this.nomTable, this.colonne, form.value.recherche).subscribe({
-          next: (reponse: Insertions) => {
-            this.envoieDonnees.emit(reponse.entry);
-          }
-        })
+        this.tableService.recherche(
+          this.nomTable,
+          this.colonne,
+          form.value.recherche
+        ).subscribe({ next: handleResponse });
       }
     }
   }
   
+
   
 }
