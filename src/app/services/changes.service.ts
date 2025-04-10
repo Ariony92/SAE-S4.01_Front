bcd74dbb8a54b9648908dea9071f5567805a24bb
Karim-Bouqsi <pro.karimb@gmail.com>
2025-04-11 00:02:01 +0200

    petite erreur réglé

--- src/app/services/changes.service.ts
+++ src/app/services/changes.service.ts
@@ -29,6 +29,6 @@ export class ChangeService {
   }
 
   initialiserDepuisScript(sql: string) {
-    return this.http.post<{ message: string }>(`${this.API_URL}/tables/init_sql`, { sql });
+    return this.http.post<{ message: string }>(`${this.API_URL}/changes/init_sql`, { sql });
   }
 }
